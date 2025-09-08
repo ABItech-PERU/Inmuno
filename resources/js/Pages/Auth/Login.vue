5e512b7f11a591b5206bb497b23e8b2533f32c83
abimaelfv <abimaelfv@hotmail.com>
2025-09-08 08:13:39 -0500

    Inicio y login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { Head, Link, useForm } from '@inertiajs/vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
+import { Link, useForm } from '@inertiajs/vue3';
 import Checkbox from '@/Components/Checkbox.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import GuestLayout from '@/Layouts/GuestLayout.vue';
 
 defineProps({
     canResetPassword: Boolean,
@@ -20,71 +19,74 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.transform(data => ({
-        ...data,
-        remember: form.remember ? 'on' : '',
-    })).post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
+    form
+        .transform(data => ({
+            ...data,
+            remember: form.remember ? 'on' : '',
+        }))
+        .post(route('login'), {
+            onFinish: () => form.reset('password'),
+        });
 };
 </script>
 
 <template>
-    <Head title="Log in" />
+    <GuestLayout title="Log in" backRoute="/">
+        <div class="w-full max-w-md mx-auto px-6 py-6">
+            <h1 class="text-2xl font-semibold text-cyan-600 mb-10">Iniciar sesión</h1>
 
-    <AuthenticationCard>
-        <template #logo>
-            <AuthenticationCardLogo />
-        </template>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-                <InputError class="mt-2" :message="form.errors.email" />
+            <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+                {{ status }}
             </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+            <form @submit.prevent="submit" class="space-y-4">
+                <div>
+                    <InputLabel for="email" value="Correo electrónico" />
+                    <TextInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus
+                        autocomplete="username" />
+                    <InputError class="mt-2" :message="form.errors.email" />
+                </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
+                <div>
+                    <InputLabel for="password" value="Contraseña" />
+                    <TextInput id="password" v-model="form.password" type="password" class="mt-1 block w-full" required
+                        autocomplete="current-password" />
+                    <InputError class="mt-2" :message="form.errors.password" />
+                </div>
+
+                <div class="text-right">
+                    <Link v-if="canResetPassword" :href="route('password.request')"
+                        class="underline text-sm font-medium text-gray-600 hover:text-cyan-600 rounded-md focus:outline-none">
+                    ¿Olvidaste tu contraseña?
+                    </Link>
+                </div>
+
+                <label class="flex items-center select-none">
                     <Checkbox v-model:checked="form.remember" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ms-2 text-sm text-gray-600">Recuérdame</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Forgot your password?
-                </Link>
+                <div class="flex items-center justify-between">
+                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                        class="w-full py-4 text-center">
+                        Iniciar sesión
+                    </PrimaryButton>
+                </div>
 
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </AuthenticationCard>
+                <!-- Continuar con Google -->
+                <div>
+                    <div class="flex items-center justify-center mt-10">
+                        <a href="/auth/google"
+                            class="flex items-center justify-center gap-3 w-full max-w-xs px-5 py-2 bg-white border border-gray-300 rounded-full shadow-md hover:shadow-lg transition duration-200 ease-in-out">
+                            <!-- Ícono de Google -->
+                            <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google"
+                                class="w-5 h-5" />
+                            <span class="text-gray-700 font-medium">Continuar con Google</span>
+                        </a>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </GuestLayout>
 </template>
